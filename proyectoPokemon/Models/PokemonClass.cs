11795491052c918ee5d107a05399175aa28b0055
unknown <alejandrojzeballos@gmail.com>
2022-03-13 17:52:27 -0400

    {add} adding attributes on pokemonClass in Models

--- proyectoPokemon/Models/PokemonClass.cs
+++ proyectoPokemon/Models/PokemonClass.cs
@@ -7,6 +7,8 @@ namespace proyectoPokemon.Models
 {
     public class PokemonClass
     {
-        
+        public int id { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
     }
 }
