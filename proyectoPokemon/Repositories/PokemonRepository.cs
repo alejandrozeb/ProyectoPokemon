c81b050b1f4f3ca9efc51cd61a9ed07ff910a8bb
unknown <alejandrojzeballos@gmail.com>
2022-03-13 18:56:49 -0400

    {add} obtainAllpokemons function implemented on PokemonRepository

--- proyectoPokemon/Repositories/PokemonRepository.cs
+++ proyectoPokemon/Repositories/PokemonRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using proyectoPokemon.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace proyectoPokemon.Repositories
 {
     public class PokemonRepository
     {
+        public String[] obtainAllPokemons()
+        {
+
+            PokemonClass pokemon = new PokemonClass();
+            //send request
+            
+            return new string[] { "pikachu", "https..." };
+        }
     }
 }
