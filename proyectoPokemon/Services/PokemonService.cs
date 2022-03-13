527148afa4cc6ca88fe809d9f4b5a6b6fdcdffe5
unknown <alejandrojzeballos@gmail.com>
2022-03-13 18:45:30 -0400

    {add} getAllpokemons function implemented on PokemonService

--- proyectoPokemon/Services/PokemonService.cs
+++ proyectoPokemon/Services/PokemonService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using proyectoPokemon.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,10 @@ namespace proyectoPokemon.Services
 {
     public class PokemonService
     {
+        public String[] getAllPokemons()
+        {
+            PokemonRepository allPokemons = new PokemonRepository();
+            return new string[] { "pikachu", "https..." };
+        }
     }
 }
