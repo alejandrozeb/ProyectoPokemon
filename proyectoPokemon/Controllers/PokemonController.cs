ae950cea4443292f84769aa78720feb38a0be9a0
unknown <alejandrojzeballos@gmail.com>
2022-03-13 18:13:12 -0400

    {fix} pokemonController changed for controller with api version

--- proyectoPokemon/Controllers/PokemonController.cs
+++ proyectoPokemon/Controllers/PokemonController.cs
@@ -1,87 +1,47 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
 namespace proyectoPokemon.Controllers
 {
-    public class PokemonController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : ControllerBase
     {
-        // GET: CountryController
-        public ActionResult Index()
+        // GET: api/<PokemonController>
+        [HttpGet]
+        public IEnumerable<string> Get()
         {
-            return View();
+            return new string[] { "value1", "value2" };
         }
 
-        // GET: CountryController/Details/5
-        public ActionResult Details(int id)
+        // GET api/<PokemonController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
         {
-            return View();
+            return "value";
         }
 
-        // GET: CountryController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CountryController/Create
+        // POST api/<PokemonController>
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public void Post([FromBody] string value)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
 
-        // GET: CountryController/Edit/5
-        public ActionResult Edit(int id)
+        // PUT api/<PokemonController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
         {
-            return View();
         }
 
-        // POST: CountryController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        // DELETE api/<PokemonController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: CountryController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: CountryController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
