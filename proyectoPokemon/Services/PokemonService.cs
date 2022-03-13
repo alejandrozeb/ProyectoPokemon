7a6b1081d65052188caee3fc70be6a42a7eecb36
unknown <alejandrojzeballos@gmail.com>
2022-03-13 17:46:50 -0400

    {add} adding pokemonService and  Pokemon class in Models

---
+++ proyectoPokemon/Services/PokemonService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace proyectoPokemon.Services
+{
+    public class PokemonService
+    {
+    }
+}
